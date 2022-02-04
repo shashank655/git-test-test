90ebd8924c7537917b2de2f27a3ba341d1b89c55
shashank655 <shashankgarg655@gmail.com>
2022-02-04 15:42:30 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelassasas12
                 </div>
 
                 <div class="links">
